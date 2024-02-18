50fc4f773fcea23a89afaf0bd4304847236d9e6f
a31361548 <a31361548@gmail.com>
2024-02-18 23:17:16 +0800

    輪播

--- vuetify-project/src/views/HomeView.vue
+++ vuetify-project/src/views/HomeView.vue
@@ -1,44 +1,47 @@
 <template>
   <v-carousel
     cycle
-    height="400"
+    height="90vh"
     hide-delimiter-background
     show-arrows="hover"
-    >
+  >
     <v-carousel-item
-    v-for="game in games"
-    :key="game._id"
+    v-for="product in products" :key="product._id"
     >
-    <v-sheet
-    height="100%"
-    v-bind="game"
-      >
-        <div class="d-flex fill-height justify-center align-center">
-         {{ name }}
-        </div>
-      </v-sheet>
+      <Article v-bind="product"></Article>
     </v-carousel-item>
   </v-carousel>
 </template>
-
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useApi } from '@/composables/axios'
+import { useSnackbar } from 'vuetify-use-dialog'
+import Article from '@/components/ArticlePost.vue'
 
-const props = defineProps(['_id', 'category', 'description', 'image', 'name', 'price', 'sell'])
-const games = ref([])
-// const colors = ref([
-//   'indigo',
-//   'warning',
-//   'pink darken-2',
-//   'red lighten-1',
-//   'deep-purple accent-4'
-// ])
+const { api } = useApi()
+const createSnackbar = useSnackbar()
+const products = ref([])
 
-// const slides = ref([
-//   'First',
-//   'Second',
-//   'Third',
-//   'Fourth',
-//   'Fifth'
-// ])
+onMounted(async () => {
+  try {
+    const { data } = await api.get('/products', {
+      params: {
+        itemsPerPage: -1
+      }
+    })
+    products.value.push(...data.result.data)
+  } catch (error) {
+    console.log(error)
+    const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+    createSnackbar({
+      text,
+      showCloseButton: false,
+      snackbarProps: {
+        timeout: 2000,
+        color: 'red',
+        location: 'bottom'
+      }
+    })
+  }
+})
 </script>
